4929a857b153e29c2c83949f6abcc6892638825c
wsydn <wsydney@web.de>
2020-06-17 18:49:27 +0200

    Topics

--- migrations/m200501_120937_elementindexes_settings.php
+++ migrations/m200501_120937_elementindexes_settings.php
@@ -39,13 +39,16 @@ class m200501_120937_elementindexes_settings extends Migration
                 ['key', 'singles'],
                 ['key', $s['page']],
                 ['key', $s['post']],
+                ['heading', 'Categories'],
+                ['key', $s['topic']],
             ],
             'sources' => [
                 '*' => ['tableAttributes' => ['section', 'postDate', 'link']],
                 'singles' => ['tableAttributes' => ['featuredImage', 'link']],
                 'drafts' => ['tableAttributes' => ['section', 'isUnsavedDraft','draftName','creatorId','dateCreated']],
                 $s['page'] => ['tableAttributes' => [$f['featuredImage'], $f['teaser'], 'postDate', 'link']],
-                $s['post'] => ['tableAttributes' => [$f['featuredImage'], $f['teaser'], 'author', 'postDate', 'link']]
+                $s['post'] => ['tableAttributes' => [$f['featuredImage'], $f['teaser'], 'author', 'postDate', 'link']],
+                $s['topic'] => ['tableAttributes' => [$f['featuredImage'], $f['teaser'], 'author', 'postDate', 'link']]
 
             ]
         ];
--- translations/de/site.php
+++ translations/de/site.php
@@ -14,6 +14,10 @@ return [
     'Pages' => 'Seiten',
     'You did not enter a search criteria' => 'Sie haben keinen Suchbegriff eingegeben',
     'All' => 'Alle',
+    'Topic' => 'Thema',
+    'Topics' => 'Themen',
+    'Other Topics' => 'Weitere Themen',
+    'By' => 'Von',
 
     'Channels' => 'Channels',
     'Structures' => 'Strukturen',
